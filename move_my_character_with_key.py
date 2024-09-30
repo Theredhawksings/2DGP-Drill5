9647041a431adcfd4348f4734669b8eaf7a02967
Theredhawksings <myeoseul@gmail.com>
2024-09-30 20:57:06 +0900

    Drill 5번 제출(2021180018,신성재)

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,13 +5,9 @@ open_canvas()
 ground = load_image('TUK_GROUND.png')
 character = load_image('run_animation.png')
 
-frame_x = [0,132,264,379,510]
-frame_widths = [132,132,115,131,119]
-frame_y = [139,20]
-frame_height = 119
 
 def handle_events():
-    global running,dirx,diry,facing_right
+    global running,dirx,diry,state
 
     events = get_events()
     for event in events:
@@ -19,14 +15,16 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
+                state = 4
                 dirx += 1
-                facing_right = True
             elif event.key == SDLK_LEFT:
+                state = 3
                 dirx -= 1
-                facing_right = False
             elif event.key == SDLK_UP:
+                state = 1
                 diry += 1
             elif event.key == SDLK_DOWN:
+                state = 2
                 diry -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
@@ -49,26 +47,24 @@ frame = 0
 dirx = 0
 diry = 0
 facing_right = True
-
+state = 1
 
 while running:
     clear_canvas()
     ground.draw(400,300)
 
-    if frame <= 4:
-        if facing_right:
-            character.clip_draw(frame_x[frame], frame_y[0], frame_widths[frame], frame_height, x, y)
-        else:
-            character.clip_composite_draw(frame_x[frame], frame_y[0], frame_widths[frame], frame_height,0, 'h', x, y, frame_widths[frame], frame_height)
-    else:
-        if facing_right:
-            character.clip_draw(frame_x[frame % 5], frame_y[0], frame_widths[frame % 5], frame_height, x, y)
-        else:
-            character.clip_composite_draw(frame_x[frame % 5], frame_y[0], frame_widths[frame % 5], frame_height, 0, 'h', x, y, frame_widths[frame % 5], frame_height)
+    if state == 1:
+        character.clip_draw(48 * frame, 0, 48, 48, x, y, 200, 200)
+    elif state == 2:
+        character.clip_draw(48 * frame, 144, 48, 48, x, y, 200, 200)
+    elif state == 3:
+        character.clip_draw(48 * frame, 96, 48, 48, x, y, 200, 200)
+    elif state == 4:
+        character.clip_draw(48 * frame, 48, 48, 48, x, y, 200, 200)
 
     update_canvas()
     handle_events()
-    frame = (frame+1)%9
+    frame = (frame+1)%2
 
     x += dirx * 10
     y += diry * 10
